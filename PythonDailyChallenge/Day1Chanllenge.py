658948d850686f93d17d96263ed47fe15ef07ac5
TM.HO <T.M.Ho@users.noreply.github.com>
2019-11-19 15:35:45 +0800

    day1 challenge

--- PythonDailyChallenge/Day1Chanllenge.py
+++ PythonDailyChallenge/Day1Chanllenge.py
@@ -1,3 +1,13 @@
-print("Day1 challenge")
+string = 'In a trimmed estimator, the extreme values are discarded; '
+new_string = []
+for i in string:
+    if i.islower():
+        new_string.append(i)
+    elif i.isalnum == False:
+        new_string.append(i)
+    elif i == ' ':
+        new_string.append(i)
 
-print('from Brian')
+string_join = ''.join(new_string)
+
+print(string_join)
